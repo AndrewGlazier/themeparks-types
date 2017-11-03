597ba44ab3dc8bdb3d705c0e93970286f4143dc7
Jonathan Harrison <j.harrison@altitudeangel.com>
2017-11-03 08:21:58 +0000

    WIP

--- types/themeparks/index.d.ts
+++ types/themeparks/index.d.ts
@@ -5,219 +5,303 @@
 
 import * as moment from "moment";
 
+// TODO Test this all
+
 export = themeparks;
 
 type ParkClass = typeof Park;
-// TODO Check these return types
-type WaitTimesResult = WaitTimes[] | null;
-type OpeningTimesResult = OpeningTimes[] | null;
+type WaitTimesResult = Ride[] | null;
+type OpeningTimesResult = ScheduleData[] | null;
 
 declare const themeparks: {
-    AllParks: ParkClass[];
-    Parks: {
-        AltonTowers: ParkClass;
-        AsterixPark: ParkClass;
-        BuschGardensTampaBay: ParkClass;
-        BuschGardensWilliamsburg: ParkClass;
-        CanadasWonderland: ParkClass;
-        Carowinds: ParkClass;
-        CedarPoint: ParkClass;
-        ChessingtonWorldOfAdventures: ParkClass;
-        DisneylandParisMagicKingdom: ParkClass;
-        DisneylandParisWaltDisneyStudios: ParkClass;
-        DisneylandResortCaliforniaAdventure: ParkClass;
-        DisneylandResortMagicKingdom: ParkClass;
-        Efteling: ParkClass;
-        EuropaPark: ParkClass;
-        HersheyPark: ParkClass;
-        HongKongDisneyland: ParkClass;
-        KingsIsland: ParkClass;
-        KnottsBerryFarm: ParkClass;
-        LaRondeMontreal: ParkClass;
-        SeaworldOrlando: ParkClass;
-        SeaworldSanAntonio: ParkClass;
-        SeaworldSanDiego: ParkClass;
-        SesamePlace: ParkClass;
-        ShanghaiDisneyResortMagicKingdom: ParkClass;
-        SixFlagsAmerica: ParkClass;
-        SixFlagsDiscoveryKingdom: ParkClass;
-        SixFlagsFiestaTexas: ParkClass;
-        SixFlagsGreatAdventure: ParkClass;
-        SixFlagsGreatAmerica: ParkClass;
-        SixFlagsHurricaneHarborArlington: ParkClass;
-        SixFlagsHurricaneHarborJackson: ParkClass;
-        SixFlagsHurricaneHarborLosAngeles: ParkClass;
-        SixFlagsMagicMountain: ParkClass;
-        SixFlagsMexico: ParkClass;
-        SixFlagsNewEngland: ParkClass;
-        SixFlagsOverGeorgia: ParkClass;
-        SixFlagsOverTexas: ParkClass;
-        SixFlagsStLouis: ParkClass;
-        SixFlagsWhiteWaterAtlanta: ParkClass;
-        TheGreatEscape: ParkClass;
-        ThorpePark: ParkClass;
-        TokyoDisneyResortDisneySea: ParkClass;
-        TokyoDisneyResortMagicKingdom: ParkClass;
-        UniversalIslandsOfAdventure: ParkClass;
-        UniversalStudiosFlorida: ParkClass;
-        UniversalStudiosHollywood: ParkClass;
-        UniversalStudiosSingapore: ParkClass;
-        UniversalVolcanoBay: ParkClass;
-        WaltDisneyWorldAnimalKingdom: ParkClass;
-        WaltDisneyWorldEpcot: ParkClass;
-        WaltDisneyWorldHollywoodStudios: ParkClass;
-        WaltDisneyWorldMagicKingdom: ParkClass;
+  AllParks: ParkClass[];
+  Parks: {
+    AltonTowers: ParkClass;
+    AsterixPark: ParkClass;
+    BuschGardensTampaBay: ParkClass;
+    BuschGardensWilliamsburg: ParkClass;
+    CanadasWonderland: ParkClass;
+    Carowinds: ParkClass;
+    CedarPoint: ParkClass;
+    ChessingtonWorldOfAdventures: ParkClass;
+    DisneylandParisMagicKingdom: ParkClass;
+    DisneylandParisWaltDisneyStudios: ParkClass;
+    DisneylandResortCaliforniaAdventure: ParkClass;
+    DisneylandResortMagicKingdom: ParkClass;
+    Efteling: ParkClass;
+    EuropaPark: ParkClass;
+    HersheyPark: ParkClass;
+    HongKongDisneyland: ParkClass;
+    KingsIsland: ParkClass;
+    KnottsBerryFarm: ParkClass;
+    LaRondeMontreal: ParkClass;
+    SeaworldOrlando: ParkClass;
+    SeaworldSanAntonio: ParkClass;
+    SeaworldSanDiego: ParkClass;
+    SesamePlace: ParkClass;
+    ShanghaiDisneyResortMagicKingdom: ParkClass;
+    SixFlagsAmerica: ParkClass;
+    SixFlagsDiscoveryKingdom: ParkClass;
+    SixFlagsFiestaTexas: ParkClass;
+    SixFlagsGreatAdventure: ParkClass;
+    SixFlagsGreatAmerica: ParkClass;
+    SixFlagsHurricaneHarborArlington: ParkClass;
+    SixFlagsHurricaneHarborJackson: ParkClass;
+    SixFlagsHurricaneHarborLosAngeles: ParkClass;
+    SixFlagsMagicMountain: ParkClass;
+    SixFlagsMexico: ParkClass;
+    SixFlagsNewEngland: ParkClass;
+    SixFlagsOverGeorgia: ParkClass;
+    SixFlagsOverTexas: ParkClass;
+    SixFlagsStLouis: ParkClass;
+    SixFlagsWhiteWaterAtlanta: ParkClass;
+    TheGreatEscape: ParkClass;
+    ThorpePark: ParkClass;
+    TokyoDisneyResortDisneySea: ParkClass;
+    TokyoDisneyResortMagicKingdom: ParkClass;
+    UniversalIslandsOfAdventure: ParkClass;
+    UniversalStudiosFlorida: ParkClass;
+    UniversalStudiosHollywood: ParkClass;
+    UniversalStudiosSingapore: ParkClass;
+    UniversalVolcanoBay: ParkClass;
+    WaltDisneyWorldAnimalKingdom: ParkClass;
+    WaltDisneyWorldEpcot: ParkClass;
+    WaltDisneyWorldHollywoodStudios: ParkClass;
+    WaltDisneyWorldMagicKingdom: ParkClass;
+  };
+  Settings: {
+    Cache: {
+      del: any;
+      get: any;
+      ignoreCacheErrors: boolean;
+      keys: any;
+      reset: any;
+      set: any;
+      store: {
+        del: any;
+        get: any;
+        keys: any;
+        name: string;
+        reset: any;
+        set: any;
+        usePromises: boolean;
+      };
+      wrap: any;
     };
-    Settings: {
-        Cache: {
-            del: any;
-            get: any;
-            ignoreCacheErrors: boolean;
-            keys: any;
-            reset: any;
-            set: any;
-            store: {
-                del: any;
-                get: any;
-                keys: any;
-                name: string;
-                reset: any;
-                set: any;
-                usePromises: boolean;
-            };
-            wrap: any;
-        };
-        DefaultCacheOpeningTimesLength: number;
-        DefaultCacheWaitTimesLength: number;
-        DefaultDateFormat: string;
-        DefaultOpenTimeout: number;
-        DefaultParkName: string;
-        DefaultParkTimeFormat: any;
-        DefaultParkTimezone: string;
-        DefaultReadTimeout: number;
-        DefaultScheduleDays: number;
-        DefaultTimeFormat: string;
-        ProxyURL: any;
-    };
+    DefaultCacheOpeningTimesLength: number;
+    DefaultCacheWaitTimesLength: number;
+    DefaultDateFormat: string;
+    DefaultOpenTimeout: number;
+    DefaultParkName: string;
+    DefaultParkTimeFormat: any;
+    DefaultParkTimezone: string;
+    DefaultReadTimeout: number;
+    DefaultScheduleDays: number;
+    DefaultTimeFormat: string;
+    ProxyURL: any;
+  };
 };
 
-// TODO test this
 // Source: https://github.com/cubehouse/themeparks/blob/master/lib/park.js
 declare class Park {
-    constructor();
-    
-    // There are actually implemented as get only Accessors but TypeScript doesn't support
-    // these in declaration files: https://github.com/Microsoft/TypeScript/issues/10969#issuecomment-248192537
-    Name: string;
-    Timezone: string;
-    Location: GeoLocation;
-    SupportsWaitTimes: boolean;
-    SupportsOpeningTimes: boolean;
-    SupportsRideSchedules: boolean;
-    FastPass: boolean;
-    FastPassReturnTimes: boolean;
-    UserAgent: string;
-    Schedule: Schedule;
-    ScheduleDays: number;
+  constructor(options?: {
+    name?: string;
+    timezone?: string;
+    timeFormat?: string;
+    dateFormat?: string;
+    cacheWaitTimesLength?: number;
+    cacheOpeningTimesLength?: number;
+    latitude?: number;
+    longitude?: number;
+    useragent?: string;
+    scheduleDaysToReturn?: number;
+  });
 
-    TimeNow: (timeFormatObject?: {
-        timeFormat?: string
-    }) => string;
+  // There are actually implemented as get only Accessors but TypeScript doesn't support
+  // these in declaration files: https://github.com/Microsoft/TypeScript/issues/10969#issuecomment-248192537
+  Name: string;
+  Timezone: string;
+  Location: GeoLocation;
+  SupportsWaitTimes: boolean;
+  SupportsOpeningTimes: boolean;
+  SupportsRideSchedules: boolean;
+  FastPass: boolean;
+  FastPassReturnTimes: boolean;
+  UserAgent: string;
+  Schedule: Schedule;
+  ScheduleDays: number;
 
-    DateNow: (dateFormatObject?: {
-        dateFormat ?: string
-    }) => string;
+  TimeNow: (
+    timeFormatObject?: {
+      timeFormat?: string;
+    }
+  ) => string;
 
-    GetWaitTimes : (callback?: (error : Error, data: WaitTimesResult) => void) => Promise<WaitTimesResult>;
+  DateNow: (
+    dateFormatObject?: {
+      dateFormat?: string;
+    }
+  ) => string;
 
-    GetWaitTimesPromise : () => Promise<WaitTimesResult>;
+  GetWaitTimes: (
+    callback?: (error: Error, data: WaitTimesResult) => void
+  ) => Promise<WaitTimesResult>;
 
-    /** 
+  GetWaitTimesPromise: () => Promise<WaitTimesResult>;
+
+  /** 
      * Fetch the ride data for the requested ID. If it doesn't exist, add a new ride to our park's ride set
      */
-    GetRideObject : (ride?: {
-        id: string;
-        name: string
-    }) => Ride;
+  GetRideObject: (
+    ride?: {
+      id: string;
+      name: string;
+    }
+  ) => Ride;
 
-    /**
+  /**
     * Fetch the ride data for the requested ID. If it doesn't exist, returns null
     */
-    FindRideObject: (ride?: {
-        id: string;     
-    }) => Ride | null;
+  FindRideObject: (
+    ride?: {
+      id: string;
+    }
+  ) => Ride | null;
 
-    GetOpeningTimes : (callback?: (error : Error, data: OpeningTimesResult) => void) => Promise<OpeningTimesResult>;
+  GetOpeningTimes: (
+    callback?: (error: Error, data: OpeningTimesResult) => void
+  ) => Promise<OpeningTimesResult>;
 
-    GetOpeningTimesPromise : () => Promise<OpeningTimesResult>;
+  GetOpeningTimesPromise: () => Promise<OpeningTimesResult>;
 }
 
-// TODO Test this works 
 // Source: https://github.com/cubehouse/themeparks/blob/master/lib/geoLocation.js
 declare class GeoLocation {
-    constructor(location: {
-        longitude?: number;
-        latitude?: number;
-    });
+  constructor(location: { longitude?: number; latitude?: number });
 
-    // There are actually implemented as get only Accessors but TypeScript doesn't support
-    // these in declaration files: https://github.com/Microsoft/TypeScript/issues/10969#issuecomment-248192537
-    Latitude : string;
-    LatitudeRaw: number;
-    Longitude: string;
-    LongitudeRaw: number;
+  // There are actually implemented as get only Accessors but TypeScript doesn't support
+  // these in declaration files: https://github.com/Microsoft/TypeScript/issues/10969#issuecomment-248192537
+  Latitude: string;
+  LatitudeRaw: number;
+  Longitude: string;
+  LongitudeRaw: number;
 
-    static RandomBetween: (locationA: GeoLocation, locationB: GeoLocation) => GeoLocation
+  static RandomBetween: (
+    locationA: GeoLocation,
+    locationB: GeoLocation
+  ) => GeoLocation;
 
-    toGoogleMaps: () => string;
-    toString: () => string;
+  toGoogleMaps: () => string;
+  toString: () => string;
 }
 
 // https://github.com/cubehouse/themeparks/blob/master/lib/schedule.js
 declare class Schedule {
-    constructor(scheduleConfig?: {
-        dateFormat?: string;
-        timeFormat ?: number;
-    });
+  constructor(scheduleConfig?: { dateFormat?: string; timeFormat?: number });
 
-    // There are actually implemented as get only Accessors but TypeScript doesn't support
-    // these in declaration files: https://github.com/Microsoft/TypeScript/issues/10969#issuecomment-248192537
-    IsDirty : boolean;
+  // There are actually implemented as get only Accessors but TypeScript doesn't support
+  // these in declaration files: https://github.com/Microsoft/TypeScript/issues/10969#issuecomment-248192537
+  IsDirty: boolean;
 
-    toJSON : () => ScheduleData2;
-    fromJSON : (scheduleData: ScheduleData2) => void;
+  toJSON: () => ScheduleRaw;
+  fromJSON: (scheduleData: ScheduleRaw) => void;
 
-    SetDate: ({
-        date? : moment.Moment | string;
-        openingTime? : moment.Moment | string;
-        closingTime? : moment.Moment | string;
-        specialHours? : boolean;
-        type? : string; 
-    }) => boolean;
+  SetDate: (
+    scheduleData: {
+      date?: moment.Moment | string;
+      openingTime?: moment.Moment | string;
+      closingTime?: moment.Moment | string;
+      specialHours?: boolean;
+      type?: string;
+    }
+  ) => boolean;
 
-    SetRange: ({
-        startDate? : moment.Moment | string;
-        endDate ? : moment.Moment | string;
-        openingTime? : moment.Moment | string;
-        closingTime? : moment.Moment | string;
-        specialHours ? : boolean;
-        type? : string; 
-    }) => boolean;
+  SetRange: (
+    scheduleData: {
+      startDate?: moment.Moment | string;
+      endDate?: moment.Moment | string;
+      openingTime?: moment.Moment | string;
+      closingTime?: moment.Moment | string;
+      specialHours?: boolean;
+      type?: string;
+    }
+  ) => boolean;
 
-    GetDate: ({
-        date? : moment.Moment | string;
-    }) => ScheduleData;
+  GetDate: (
+    dateDate: {
+      date?: moment.Moment | string;
+    }
+  ) => ScheduleData | boolean;
 
-    GetDateRange: ({
-        startDate? : moment.Moment | string;
-        endDate? : moment.Moment | string;
-    }) => ScheduleData;
+  GetDateRange: (
+    dateData: {
+      startDate?: moment.Moment | string;
+      endDate?: moment.Moment | string;
+    }
+  ) => ScheduleData[];
+}
+
+declare interface ScheduleRaw {
+  dates: Map<number, ScheduleData>;
+  datesSpecial: Map<number, SpecialScheduleData[]>;
 }
 
 declare interface ScheduleData {
-    date : moment.Moment;
-    openingTime : moment.Moment;
-    closingTime : moment.Moment;
-    type : string; 
-    special: ;
+  date: moment.Moment;
+  openingTime: moment.Moment;
+  closingTime: moment.Moment;
+  type: string;
+  special?: SpecialScheduleData[];
+}
+
+declare interface SpecialScheduleData {
+  openingTime: moment.Moment;
+  closingTime: moment.Moment;
+  type: string;
+}
+
+// https://github.com/cubehouse/themeparks/blob/master/lib/ride.js
+declare class Ride {
+  constructor(options: { ride_id?: string; ride_name?: number });
+
+  // There are actually implemented as get only Accessors but TypeScript doesn't support
+  // these in declaration files: https://github.com/Microsoft/TypeScript/issues/10969#issuecomment-248192537
+  Name: string;
+  WaitTime: number;
+  FastPass: boolean;
+  FastPassReturnTimeAvailable: boolean;
+  // The set for this is a moment
+  FastPassReturnTimeStart: string;
+  // The set for this is a moment.
+  FastPassReturnTimeEnd: string;
+  Active: boolean;
+  Status: RideStatus;
+  LastUpdate: number | undefined;
+  Schedule: Schedule;
+
+  toJSON: () => RideData;
+  fromJSON: (rideData: RideData) => void;
+}
+
+declare interface RideData {
+  id: string;
+  name: string;
+  active: boolean;
+  waitTime: number;
+  fastPass: boolean;
+  lastUpdate: number | undefined;
+  status: RideStatus;
+  fastPassReturnTime?: {
+    startTime: string;
+    endTime: string;
+    lastUpdate: number | undefined;
+  };
+  schedule: ScheduleData;
+}
+
+declare enum RideStatus {
+  "Operating",
+  "Down",
+  "Closed",
+  "Refurbishment"
 }
